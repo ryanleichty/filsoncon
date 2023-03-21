ff7258fb6f15df49d94049b845f6f07913f4f08e
Ryan Leichty <ryan.leichty@gmail.com>
2023-03-21 13:10:14 -0700

    Update button component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,17 +1,27 @@
+import Link from "next/link";
+import { forwardRef } from "react";
 import { cx } from "~/utils";
 
-export function Button({
-  color = "primary",
-  size = "base",
-  icon: Icon,
-  children,
-  className,
-  ...props
-}) {
+export const Button = forwardRef(function Button(
+  {
+    color = "primary",
+    size = "base",
+    icon: Icon,
+    isLoading,
+    loadingText = "Loading...",
+    children,
+    className,
+    ...props
+  },
+  ref
+) {
+  const Component = props?.href ? Link : "button";
+
   const baseStyles =
-    "inline-flex items-center gap-3 uppercase font-bold tracking-wide transition-colors whitespace-nowrap border border-transparent text-xs text-center outline-none focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-black duration-100";
+    "inline-flex leading-none items-center justify-center gap-3 uppercase font-bold tracking-wide transition-colors whitespace-nowrap border border-transparent text-xs text-center outline-none focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-4 focus-visible:outline-black duration-100";
 
   const sizeStyles = {
+    xs: "h-8 px-3 text-xs tracking-widest",
     sm: "h-12 px-6 text-sm",
     base: "h-14 sm:h-16 px-6 sm:px-8 text-sm sm:text-base",
   };
@@ -19,11 +29,13 @@ export function Button({
   const colorStyles = {
     primary: "bg-green-700 text-antique-100 hover:bg-green-800",
     secondary: "bg-black text-antique-100",
-    white: "bg-antique-100 text-black",
+    white: "bg-antique-100 text-black hover:bg-antique-300",
   };
 
   return (
-    <button
+    <Component
+      ref={ref}
+      type={props?.href ? null : "button"}
       className={cx(
         baseStyles,
         sizeStyles[size],
@@ -33,7 +45,7 @@ export function Button({
       {...props}
     >
       {Icon && <Icon size={size} />}
-      {children}
-    </button>
+      {isLoading ? loadingText : children}
+    </Component>
   );
-}
+});
